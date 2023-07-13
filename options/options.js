43d3d60dc897ac4347a30883cc3ebe191ab761f7
Crowdbotics <team@crowdbotics.com>
2023-07-13 13:31:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nyfa-community-42140",
+  url: "https://nyfa-community-42140.botics.co",
+  api: "https://nyfa-community-42140.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
